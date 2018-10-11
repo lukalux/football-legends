dab48dacd40004b5bb9c8ddb39db11443b9ad82b
root <root@staging.razvrstaj.me>
2018-10-11 09:41:18 +0000

    again changed position of wsgi

--- legends/wsgi.py
+++ legends/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append("/home/legends/djangosites/legends")
+sys.path.append("/home/legends/djangosites/legends/legends")
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "legends.settings")
 
 application = get_wsgi_application()
+
